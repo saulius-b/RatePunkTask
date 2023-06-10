67df04d7da6728640416a5501ccceba8c74671fd
Saulius Balčiūnas <saulius.balciunas@barbora.lt>
2023-06-11 00:09:00 +0300

    header styling complete

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import logo from "../../../assets/logo.svg";
+import menu from "../../../assets/menu.svg";
+import { useMediaQuery } from "react-responsive";
 
-const Header = () => (
-  <header className={styles.container}>
-    <img src={logo} className={styles.imgContainer}></img>
+const Header = () => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const navType = isMobile ? (
+    <img src={menu} alt="Menu" />
+  ) : (
     <nav className={styles.navContainer}>
       <a href="">Chrome Extension</a>
       <a href="">Price Comparison</a>
       <a href="">Blog</a>
     </nav>
-  </header>
-);
+  );
+  return (
+    <header className={styles.container}>
+      <img src={logo} className={styles.imgContainer} alt="Logo"></img>
+      {navType}
+    </header>
+  );
+};
 
 export default Header;
