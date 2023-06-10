29d87e07a33a09d923615e3242b7f8e5e69c27f1
Saulius Balčiūnas <saulius.balciunas@barbora.lt>
2023-06-10 21:58:21 +0300

    added normalize scss

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Header from "./components/header/Header";
 import MainSection from "./components/main/MainSection ";
 import Footer from "./components/footer/Footer";
+import "./index.scss";
 
 const App: React.FC = () => {
   return (
