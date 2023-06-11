69bb8b35bf53d012a8ae35f66d38020d7eca7505
Saulius Balčiūnas <saulius.balciunas@barbora.lt>
2023-06-12 00:30:18 +0300

    mobile navigation complete

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./header.module.scss";
 import logo from "../../../assets/svg/logo.svg";
 import menu from "../../../assets/svg/menu.svg";
+import close from "../../../assets/svg/close.svg";
 import { useMediaQuery } from "react-responsive";
 
 const Header = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+  };
+
   const navType = isMobile ? (
-    <img src={menu} alt="Menu" />
+    <>
+      <img src={isMenuOpen ? close : menu} alt="Menu" onClick={handleMenuToggle} />
+      {isMenuOpen && (
+        <div className={styles.openedNavMenu}>
+          <div className={styles.buttonRow}>
+            <img src={logo} alt="logo"></img>
+            <img src={isMenuOpen ? close : menu} alt="menu" onClick={handleMenuToggle} />
+          </div>
+          <nav className={styles.navigationItemContainer}>
+            <a href="">Chrome Extension</a>
+            <a href="">Price Comparison</a>
+            <a href="">Blog</a>
+          </nav>
+        </div>
+      )}
+    </>
   ) : (
     <nav className={styles.navContainer}>
       <a href="" className={styles.firstNavItem}>
@@ -17,6 +39,7 @@ const Header = () => {
       <a href="">Blog</a>
     </nav>
   );
+
   return (
     <header className={styles.container}>
       <img src={logo} className={styles.imgContainer} alt="Logo"></img>
