54a8ea321e4ca83df1041485e76cb3a42ee3b25b
Saulius Balčiūnas <saulius.balciunas@barbora.lt>
2023-06-11 19:26:40 +0300

    desktop version footer styling complete

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,70 +7,85 @@ import facebook from "../../../assets/svg/facebook.svg";
 import linkedin from "../../../assets/svg/linkedin.svg";
 import twitter from "../../../assets/svg/twitter.svg";
 import tiktok from "../../../assets/svg/tiktok.svg";
+import { useMediaQuery } from "react-responsive";
 
-const Footer = () => (
-  <footer className={styles.container}>
-    <div>
-      <img src={logo} alt="logo"></img>
-      <div className={styles.ratepunkDescription}>
-        Ratepunk compares hotel room prices across the major online travel
-        agencies. When you search for a room, Ratepunk extension scans the top
-        booking sites and runs a price comparison, so you can be confident in
-        knowing you’re getting the best deal!
+const Footer = () => {
+  const isDesktop = useMediaQuery({ minWidth: 1200 });
+  return (
+    <footer className={styles.container}>
+      <div className={styles.descriptionContainer}>
+        <img src={logo} alt="logo"></img>
+        <div className={styles.ratepunkDescription}>
+          Ratepunk compares hotel room prices across the major online travel
+          agencies. When you search for a room, Ratepunk extension scans the top
+          booking sites and runs a price comparison, so you can be confident in
+          knowing you’re getting the best deal!
+        </div>
+        {isDesktop && (
+          <div className={styles.desktopWatermark}>
+            © 2021 Ratepunk. All Rights Reserved.
+          </div>
+        )}
       </div>
-    </div>
-    <div className={styles.quickLinksContainer}>
-      <div className={styles.headerStyling}>QUICK LINKS</div>
-      <div className={styles.quickLinksAnchorContainer}>
-        <a href="#" target="_blank" className={styles.quickLinksItem}>
-          Price Comparison
-        </a>
-        <a href="#" target="_blank" className={styles.quickLinksItem}>
-          Chrome Extension
-        </a>
-        <a href="#" target="_blank" className={styles.quickLinksItem}>
-          How It Works
-        </a>
-        <a href="#" target="_blank" className={styles.quickLinksItem}>
-          Ratepunk Blog
-        </a>
-        <a href="#" target="_blank" className={styles.quickLinksItem}>
-          Privacy Policy
-        </a>
+      <div className={styles.quickLinksContainer}>
+        <div className={styles.headerStyling}>QUICK LINKS</div>
+        <div className={styles.quickLinksAnchorContainer}>
+          <a href="#" target="_blank" className={styles.quickLinksItem}>
+            Price Comparison
+          </a>
+          <a href="#" target="_blank" className={styles.quickLinksItem}>
+            Chrome Extension
+          </a>
+          <a href="#" target="_blank" className={styles.quickLinksItem}>
+            How It Works
+          </a>
+          <a href="#" target="_blank" className={styles.quickLinksItem}>
+            Ratepunk Blog
+          </a>
+          <a href="#" target="_blank" className={styles.quickLinksItem}>
+            Privacy Policy
+          </a>
+        </div>
       </div>
-    </div>
-    <div>
-      <div className={styles.headerStyling}>CONTACT</div>
-      <div className={styles.emailContainer}>
-        <img src={email}></img>
-        <a href="mailto:hi@ratepunk.com" className={styles.email}>
-          hi@ratepunk.com
-        </a>
-      </div>
+      <div>
+        <div>
+          <div className={styles.headerStyling}>CONTACT</div>
+          <div className={styles.emailContainer}>
+            <img src={email}></img>
+            <a href="mailto:hi@ratepunk.com" className={styles.email}>
+              hi@ratepunk.com
+            </a>
+          </div>
+        </div>
+        <div className={styles.socialContainer}>
+          <div className={styles.headerStyling}>SOCIAL</div>
+          <div className={styles.socialIconsContainer}>
+            <a href="#" target="_blank" className={styles.socialIconContainer}>
+              <img src={instagram} alt="instagram"></img>
+            </a>
+            <a href="#" target="_blank" className={styles.socialIconContainer}>
+              <img src={facebook} alt="facebook"></img>
+            </a>
+            <a href="#" target="_blank" className={styles.socialIconContainer}>
+              <img src={linkedin} alt="linkedin"></img>
+            </a>
+            <a href="#" target="_blank" className={styles.socialIconContainer}>
+              <img src={twitter} alt="twitter"></img>
+            </a>
+            <a href="#" target="_blank" className={styles.socialIconContainer}>
+              <img src={tiktok} alt="tiktok"></img>
+            </a>
+          </div>
+        </div>
 
-      <div className={styles.headerStyling}>SOCIAL</div>
-      <div className={styles.socialIconsContainer}>
-        <a href="#" target="_blank" className={styles.socialIconContainer}>
-          <img src={instagram} alt="instagram"></img>
-        </a>
-        <a href="#" target="_blank" className={styles.socialIconContainer}>
-          <img src={facebook} alt="facebook"></img>
-        </a>
-        <a href="#" target="_blank" className={styles.socialIconContainer}>
-          <img src={linkedin} alt="linkedin"></img>
-        </a>
-        <a href="#" target="_blank" className={styles.socialIconContainer}>
-          <img src={twitter} alt="twitter"></img>
-        </a>
-        <a href="#" target="_blank" className={styles.socialIconContainer}>
-          <img src={tiktok} alt="tiktok"></img>
-        </a>
+        {!isDesktop && (
+          <div className={styles.watermark} id="watermark">
+            © 2021 Ratepunk. All Rights Reserved.
+          </div>
+        )}
       </div>
-      <div className={styles.watermark}>
-        © 2021 Ratepunk. All Rights Reserved.
-      </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
