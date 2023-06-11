b9553fbe30c1ba716ca3bd4b5196f0fa925ef2db
Saulius Balčiūnas <saulius.balciunas@barbora.lt>
2023-06-11 09:54:10 +0300

    initial main section styling added

--- src/components/main/MainSection .tsx
+++ src/components/main/MainSection .tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./MainSection.module.scss";
 
-const MainSection = () => <main className={styles.container}>Main</main>;
+const MainSection = () => (
+  <main className={styles.container}>
+    <div>rewards</div>
+    <div>instructions</div>
+  </main>
+);
 
 export default MainSection;
