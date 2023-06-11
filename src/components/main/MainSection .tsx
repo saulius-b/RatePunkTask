3f6d5827c004ca197915ba6419c36e50b7ffda40
Saulius Balčiūnas <saulius.balciunas@barbora.lt>
2023-06-11 11:12:10 +0300

    initial refferal section styling complete

--- src/components/main/MainSection .tsx
+++ src/components/main/MainSection .tsx
@@ -3,7 +3,22 @@ import styles from "./MainSection.module.scss";
 
 const MainSection = () => (
   <main className={styles.container}>
-    <div>rewards</div>
+    <div className={styles.referralContainer}>
+      <div>
+        <div className={styles.referralTextContainer}>
+          <div className={styles.referralTextHeader}>
+            REFER FRIENDS AND GET REWARDS
+          </div>
+          <div className={styles.referralTextBody}>
+            {`Refer your friends to us and earn hotel booking vouchers. We'll give
+            you 1 coin for each friend that installs our extension. Minimum
+            cash-out at 20 coins.`}
+          </div>
+        </div>
+        <div className={styles.emailEntryContainer}>email entry</div>
+      </div>
+      <div className={styles.referralNote}>Limits on max rewards apply.</div>
+    </div>
     <div>instructions</div>
   </main>
 );
